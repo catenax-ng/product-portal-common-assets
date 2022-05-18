2f3065a115eaed3f3b989179cd31969a483958b3
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2022-05-18 16:05:17 +0200

    added swagger with auth

---
+++ public/swagger/swagger-initializer.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  //url: "https://petstore.swagger.io/v2/swagger.json",
+  window.ui = SwaggerUIBundle({
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout",
+    configUrl: "/swagger/api/swagger-config.json",
+    //validatorUrl: ""
+  });
+  window.ui.initOAuth({
+    "clientId":"catenax-portal",
+    "scopes":"openid profile",
+    "usePkceWithAuthorizationCodeGrant":"true"
+  });
+  //</editor-fold>
+};
